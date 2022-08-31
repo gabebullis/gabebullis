d50245cf4e17a2027a75058d9da27f2670a47bc4
gabebullis <90726806+gabebullis@users.noreply.github.com>
2022-08-31 18:15:55 -0400

    Kinematics
    I am struggling to find out why "u" isn't defined. Or at least, why I can't figure out how to let my computer solve for it.

---
+++ physics_kinematics.py
@@ -0,0 +1,17 @@
+print("If value is unknown, then type "u" for its value")
+
+a = input("Input Aceleration: ")
+vi = input("Input initial velocity: ")
+vf = input("Input final velocity: ")
+xi = input("Input initial x-value: ")
+xf = input("Input final x-value: ")
+t = input("Input final time: ")
+# t = float(input("Input final time: "))
+
+
+if a = u and xi = u and xf = u:
+    a = (vf-vi)/t
+# if a == u and t == u:
+    # t = (xf-xi)/(0.5*(vi+vf))
+    # float(t)
+    # print(t)
